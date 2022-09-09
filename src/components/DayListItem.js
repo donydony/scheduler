c62bf0367bf7f96d2d4d7bf9a6db67b59d719aa5
Donny Zhao <donny.zh@hotmail.com>
2022-09-09 14:10:39 -0400

    implement interview list and item

---
+++ src/components/DayListItem.js
@@ -0,0 +1,16 @@
+import React from "react";
+import classNames from 'classnames/bind';
+
+import "components/DayListItem.scss"
+
+export default function DayListItem(props) {
+  const className = classNames('dayClass', 'day-list__item', {'day-list__item--selected': props.selected, 'day-list__item--full': (props.spots === 0)});
+  return (
+    <li onClick={() => props.setDay(props.name)} className={className}>
+      <h2 className={"text--regular"}>{props.name}</h2>
+      {(props.spots === 0) && <h3 className="text--light">no spots remaining</h3>}
+      {(props.spots === 1) && <h3 className="text--light">{props.spots} spot remaining</h3>}
+      {(props.spots > 1) && <h3 className="text--light">{props.spots} spots remaining</h3>}
+    </li>
+  );
+}
---
+++ src/components/InterviewerListItem.js
@@ -0,0 +1,29 @@
+import React from "react";
+import classNames from 'classnames/bind';
+
+import "components/InterviewerListItem.scss"
+
+export default function InterviewerListItem(props) {
+  let classNameList = "interviewers__item";
+  if (props.selected) {
+    classNameList += "--selected"
+  }
+  const classNameImg = classNames(
+    'interviewers__item-image',
+    { 'interviewers__item--selected-image': props.selected }
+  );
+
+  return (
+    <li
+      onClick={() => props.setInterviewer}
+      className={classNameList}
+    >
+      <img
+        className={classNameImg}
+        src={props.avatar}
+        alt={props.name}
+      />
+      {props.selected && props.name}
+    </li>
+  );
+}
