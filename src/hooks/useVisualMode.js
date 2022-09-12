7a14824299b8d43c8261185839692a367f4703df
Donny Zhao <donny.zh@hotmail.com>
2022-09-12 03:35:59 -0400

    implement useVisualMode

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,29 @@
+import { useState } from "react";
+
+export default function useVisualMode(initial) {
+  const [mode, setValue] = useState(initial);
+  const [history, setHistory] = useState([initial]);
+
+  const transition = function (trans, replace = false) {
+    if (replace) {
+      setHistory(curr => [...curr.slice(0, curr.length - 1), trans]);
+    } else {
+      setHistory([...history, trans]);
+    }
+    setValue(trans);
+    console.log('history: ', history, 'trans: ', trans)
+  };
+
+  const back = function () {
+    if (history.length > 1) {
+      setValue(history[history.length - 2]);
+      setHistory(curr => curr.slice(0, curr.length - 1));
+    }
+  };
+
+  return {
+    mode,
+    transition,
+    back
+  };
+}
