6d7866220e88d26135acdaddc150cfd4f28330f4
Donny Zhao <donny.zh@hotmail.com>
2022-09-19 22:53:52 -0400

    add application test to improve coverage

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -6,7 +6,7 @@ import "components/DayListItem.scss"
 export default function DayListItem(props) {
   const className = classNames('dayClass', 'day-list__item', {'day-list__item--selected': props.selected, 'day-list__item--full': (props.spots === 0)});
   return (
-    <li onClick={() => props.setDay(props.name)} className={className}>
+    <li onClick={() => props.setDay(props.name)} className={className} data-testid="day">
       <h2 className={"text--regular"}>{props.name}</h2>
       {(props.spots === 0) && <h3 className="text--light">no spots remaining</h3>}
       {(props.spots === 1) && <h3 className="text--light">{props.spots} spot remaining</h3>}
